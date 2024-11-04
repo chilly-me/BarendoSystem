56a0e4249bb24c285882b229156c5b68a94a95c4
chilly-me <calebchebii9@gmail.com>
2024-11-04 17:58:45 +0300

    Finished with django-auth, heading to start using google auth

--- BarendoSystems/urls.py
+++ BarendoSystems/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('authenticate/', include('CAuthentication.urls') )
+    path('', include('CAuthentication.urls') )
 ]
--- CAuthentication/urls.py
+++ CAuthentication/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
-app_name='CAuthentication'
+app_name = 'CAuthentication'
 
 urlpatterns = [
-    path('login/', views.login_page, name='login')
+    path('', views.home, name='home'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('signup/', views.signup_page, name='signup'),
+    path('profile/', views.profile, name='profile'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
