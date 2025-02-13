3829d124337c980115e05d2f27305ded0291a564
chillyme <calebchebii9@gmail.com>
2025-02-13 10:10:14 +0300

    Heading to resolve Mpesa payments

--- payment/urls.py
+++ payment/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name='payment'
 
 urlpatterns = [
-    path('mpesa/', views.mpesaPayment, name='mpesa'),
+    path('mpesa-page/', views.mpesa_page, name='mpesa_page'),
+    path('mpesa-payment/', views.mpesaPayment, name='mpesa_payment'),
     path('paypal/<int:order_id>/', views.paypalPayment, name='paypal'),
     path('paymentSuccess/<int:order_id>/', views.paymentSuccess, name="payment-success"),
     path('paymentFail/<int:order_id>/', views.paymentPaymentFailure, name="payment-failure"),
